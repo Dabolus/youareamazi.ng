62d5deff5fc995b7ddc937540fbe1f8465e5bc69
Giorgio Garasto <giorgio@garasto.it>
2020-04-01 20:31:42 +0200

    refactor: use a single webpack config

--- src/middlewares/dev.ts
+++ src/middlewares/dev.ts
@@ -1,9 +1,6 @@
 import path from 'path';
-import Koa from 'koa';
 import koaWebpack from 'koa-webpack';
-import ejs from 'koa-ejs';
-import send from 'koa-send';
-import webpack from 'webpack';
+import webpack, { Compiler } from 'webpack';
 import webpackConfigs from '../../webpack.config';
 import { Middleware } from 'koa';
 import health from './health';
@@ -12,50 +9,76 @@ import decode from '../helpers/decode';
 const configureDevMiddlewares = async () => {
   const [homeConfig, motivationConfig] = webpackConfigs;
 
-  const [homeMiddleware, motivationMiddleware] = await Promise.all([
-    koaWebpack({ compiler: webpack(homeConfig) }),
-    koaWebpack({ compiler: webpack(motivationConfig) }),
-  ]);
+  const webpackMiddleware = await koaWebpack({
+    compiler: (webpack(webpackConfigs) as unknown) as Compiler,
+    devMiddleware: {
+      publicPath: '/',
+    },
+  });
 
   const nameMiddleware: Middleware = async (ctx, next) => {
-    if (ctx.request.path.startsWith('/motivation/')) {
-      ctx.state = {
-        ...ctx.state,
-        /* EXAMPLE RESULT */
-        // Request path:         /motivation/foo.bar
-        // RESULT:               name = 'Foo Bar'
-        name: decode(ctx.request.path.slice(12)),
-      };
+    if (!ctx.request.path.startsWith('/motivation')) {
+      return next();
     }
+
+    const homeFiles = webpackMiddleware.devMiddleware.fileSystem.readdirSync(
+      homeConfig.output!.path!,
+    );
+    const motivationFiles = webpackMiddleware.devMiddleware.fileSystem.readdirSync(
+      motivationConfig.output!.path!,
+    );
+
+    if (
+      homeFiles.some(file => ctx.request.path.includes(file)) ||
+      motivationFiles.some(file => ctx.request.path.includes(file))
+    ) {
+      return next();
+    }
+
+    ctx.state = {
+      ...ctx.state,
+      /* EXAMPLE RESULT */
+      // Request path:         /motivation/foo.bar
+      // RESULT:               name = 'Foo Bar'
+      name: decode(ctx.request.path.slice(12)),
+    };
 
     return next();
   };
 
-  const renderMiddleware: Middleware = async ctx => {
-    if (ctx.request.path.startsWith('/home')) {
-      const index = path.resolve(homeConfig.output!.path!, 'index.html');
-
-      ctx.response.type = 'html';
-      ctx.response.body = homeMiddleware.devMiddleware.fileSystem.createReadStream(
-        index,
-      );
-
-      return;
+  const renderMiddleware: Middleware = async (ctx, next) => {
+    if (!ctx.request.path.startsWith('/motivation')) {
+      return next();
     }
 
-    if (!ctx.request.path.startsWith('/motivation') || !('name' in ctx.state)) {
-      ctx.response.redirect('/home');
-      return;
+    const homeFiles = webpackMiddleware.devMiddleware.fileSystem.readdirSync(
+      homeConfig.output!.path!,
+    );
+    const motivationFiles = webpackMiddleware.devMiddleware.fileSystem.readdirSync(
+      motivationConfig.output!.path!,
+    );
+
+    if (
+      homeFiles.some(file => ctx.request.path.includes(file)) ||
+      motivationFiles.some(file => ctx.request.path.includes(file))
+    ) {
+      return next();
     }
+
+    const indexToRender = path.resolve(
+      motivationConfig.output!.path!,
+      'index.ejs',
+    );
+
+    const template = webpackMiddleware.devMiddleware.fileSystem.readFileSync(
+      indexToRender,
+      'utf8',
+    );
+
+    await ctx.render(template);
   };
 
-  return [
-    health(),
-    homeMiddleware,
-    motivationMiddleware,
-    nameMiddleware,
-    renderMiddleware,
-  ];
+  return [health(), webpackMiddleware, nameMiddleware, renderMiddleware];
 };
 
 export default configureDevMiddlewares;
