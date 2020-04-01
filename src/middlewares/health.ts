c671d232160e9bc4d8e250ccea6cc0ed7e76cd9a
Giorgio Garasto <giorgio@garasto.it>
2020-04-01 18:44:02 +0200

    fix: corrrectly call next middlewares on health middleware

--- src/middlewares/health.ts
+++ src/middlewares/health.ts
@@ -1,10 +1,12 @@
 import { Middleware } from 'koa';
 
-const health: () => Middleware = () => async ctx => {
+const health: () => Middleware = () => async (ctx, next) => {
   if (ctx.path === '/health') {
     ctx.status = 204;
     return;
   }
+
+  return next();
 };
 
 export default health;
