e5808ed35c1f846a5895d21638383cbed0342715
Giorgio Garasto <giorgio@garasto.it>
2020-04-02 08:21:21 +0200

    fix: don't set root in dev mode

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,25 @@ import configureProdMiddlewares from './middlewares/prod';
 import configureDevMiddlewares from './middlewares/dev';
 
 const hostname = 'youareamazi.ng';
+const isDev = process.env.NODE_ENV !== 'production';
 
 const start = () =>
   new Promise(async (resolve, reject) => {
     const port = process.env.PORT || 4416;
     const app = new Koa();
 
-    ejs(app, {
-      root: path.resolve(__dirname, 'apps/motivation'),
-    });
+    ejs(
+      app,
+      isDev
+        ? undefined
+        : {
+            root: path.resolve(__dirname, 'apps/motivation'),
+          },
+    );
 
-    const middlewares =
-      process.env.NODE_ENV === 'production'
-        ? await configureProdMiddlewares(hostname)
-        : await configureDevMiddlewares();
+    const middlewares = isDev
+      ? await configureDevMiddlewares()
+      : await configureProdMiddlewares(hostname);
 
     middlewares.forEach(middleware => app.use(middleware));
 
