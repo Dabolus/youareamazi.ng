bf402feb31cc3f7b05858b94c12f950e2aee89d9
Giorgio Garasto <giorgio@garasto.it>
2020-04-02 22:35:19 +0200

    fix: handle punycode decoding errors

--- src/helpers/decode.ts
+++ src/helpers/decode.ts
@@ -4,8 +4,13 @@ const decode = (encoded: string) =>
   encoded
     .split('.')
     .map(word => {
-      const decoded = decodeFromPunycode(word);
-      return decoded.charAt(0).toUpperCase() + decoded.slice(1);
+      let decodedWord = word;
+
+      try {
+        decodedWord = decodeFromPunycode(word);
+      } catch {}
+
+      return decodedWord.charAt(0).toUpperCase() + decodedWord.slice(1);
     })
     .join(' ');
 
