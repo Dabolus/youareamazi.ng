6ab7711d6ba32b9539490f2fe73ea0864de11e44
Giorgio Garasto <giorgio@garasto.it>
2020-04-02 08:35:28 +0200

    feat: implement motivation app

---
+++ src/apps/motivation/index.ts
@@ -0,0 +1,65 @@
+import phrases from './phrases.yaml';
+import { shuffle, fitText } from './helpers';
+import WebFont from 'webfontloader';
+import './main.scss';
+
+const shuffledPhrases = shuffle(phrases);
+const textElement = document.querySelector<HTMLDivElement>('#text');
+const authorElement = document.querySelector<HTMLDivElement>('#author');
+const name = document.querySelector<HTMLHeadingElement>('#name');
+let domReady = false;
+let fontsReady = false;
+let index = 0;
+
+const loop = () => {
+  textElement.className = 'hidden';
+  authorElement.className = 'hidden';
+
+  setTimeout(() => {
+    const {
+      [index]: [text, author],
+    } = shuffledPhrases;
+
+    textElement.textContent = text;
+    authorElement.textContent = author;
+
+    textElement.className = '';
+    authorElement.className = '';
+
+    index = (index + 1) % shuffledPhrases.length;
+  }, 1000);
+};
+
+const start = () => {
+  if (!domReady || !fontsReady) {
+    return;
+  }
+
+  fitText(name);
+  document.body.className = '';
+
+  setTimeout(() => {
+    loop();
+    setInterval(loop, 12000);
+  }, 9000);
+};
+
+WebFont.load({
+  google: {
+    families: ['Product Sans', 'Roboto', 'Homemade Apple'],
+  },
+  active: () => {
+    fontsReady = true;
+    start();
+  },
+});
+
+if (typeof window !== 'undefined' && window.document) {
+  domReady = true;
+  start();
+} else {
+  window.onload = () => {
+    domReady = true;
+    start();
+  };
+}
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,13 @@
+declare module '*.yaml' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.yml' {
+  const content: any;
+  export default content;
+}
+
 declare module '*.scss' {
   const content: string;
   export default content;
