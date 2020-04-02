f001b70cc93d9e2eefe2af4f90b6f619f6f71da1
Giorgio Garasto <giorgio@garasto.it>
2020-04-02 14:11:01 +0200

    feat: improve home page name to url encoding

--- src/apps/home/index.ts
+++ src/apps/home/index.ts
@@ -1,31 +1,39 @@
 import WebFont from 'webfontloader';
 import './main.scss';
+import { encode as encodeToPunycode } from 'punycode';
 
 const name = document.querySelector<HTMLInputElement>('#name');
+
+const encode = (pretty: string) =>
+  pretty
+    .trim()
+    .replace(/\s+/g, ' ')
+    .split(' ')
+    .map(word => encodeToPunycode(word.toLowerCase()))
+    .join('.');
 
 const redirect = () => {
   if (name.value) {
     document.body.className = 'loading';
-    setTimeout(function() {
-      window.location.href =
-        'http://' +
-        name.value
-          .replace(/^\s+|\s+$/g, '')
-          .replace(/\./g, '')
-          .replace(/ /g, '.') +
-        '.youareamazi.ng';
+    setTimeout(() => {
+      /* EXAMPLE RESULT */
+      // Input value:          Foo Bar
+      // RESULT:               https://foo.bar.youareamazi.ng
+      window.location.href = `https://${encode(name.value)}.youareamazi.ng`;
     }, 2000);
   } else {
     name.className = 'shaking';
-    setTimeout(function() {
+    setTimeout(() => {
       name.className = '';
     }, 820);
   }
 };
 
 const start = () => {
-  name.addEventListener('keydown', function(e) {
-    if (e.which === 13) redirect();
+  name.addEventListener('keydown', event => {
+    if (event.key === 'Enter') {
+      redirect();
+    }
   });
   document.getElementById('start').addEventListener('click', redirect);
 };
