71ff625bdeb19954962003bceab3026894d3cf38
Giorgio Garasto <giorgio@garasto.it>
2020-04-03 13:58:27 +0200

    feat: use dashes instead of underscores as spaces

--- src/helpers/decode.ts
+++ src/helpers/decode.ts
@@ -1,19 +1,26 @@
 import { decode as decodeFromPunycode } from 'punycode';
 
-const decode = (encoded: string) =>
-  encoded
-    .split('_')
-    .map(word => {
-      let decodedWord = word;
+const decode = (encoded: string) => {
+  let decodedString = encoded;
 
-      if (decodedWord.startsWith('xn--')) {
-        try {
-          decodedWord = decodeFromPunycode(word.slice(4));
-        } catch {}
-      }
+  // If the string is an IDN, then we need to decode it using punycode
+  if (decodedString.startsWith('xn--')) {
+    try {
+      decodedString = decodeFromPunycode(encoded.slice(4));
+    } catch {}
+  }
 
-      return decodedWord.charAt(0).toUpperCase() + decodedWord.slice(1);
-    })
-    .join(' ');
+  return (
+    decodedString
+      // Split the string into words separated by a dash
+      .split('-')
+      // Remove the empty words (i.e. when there are multiple adjacent dashes)
+      .filter(Boolean)
+      // Capitalize each word
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      // Join them back together with a space
+      .join(' ')
+  );
+};
 
 export default decode;
